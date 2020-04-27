9e5bb7db4e6b66012005552a8e2ca23728250358
李连成 <liancheng.li@yunhutech.com>
2020-04-27 14:19:48 +0800

    'update'

---
+++ src/component/Filter.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function Filter(){
+
+  return(
+    <div>我是Filter</div>
+  )
+}
+export default Filter
---
+++ src/project/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Filter from '../../component/Filter.jsx'
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Filter />
+        <div>Home就是我</div>
+      </div>
+    )
+  }
+}
