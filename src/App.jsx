fde0b72937d6ebd110f646b3902a0b9ae0d68fc1
李连成 <liancheng.li@yunhutech.com>
2020-04-27 14:38:58 +0800

    'router完成'

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import {
   Route,
   Link
 } from "react-router-dom";
+
 import Home from './project/home'
+import About from './project/about'
+
 function App() {
   return (
     <Router>
@@ -15,12 +18,16 @@ function App() {
             <li>
               <Link to="/">Home</Link>
             </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
         <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
---
+++ src/project/about/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Filter from '../../component/Filter.jsx'
+export default class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <Filter />
+        <div>about就是我</div>
+      </div>
+    )
+  }
+}
